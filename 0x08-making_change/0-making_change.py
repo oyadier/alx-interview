a31eae85df46199a3eabdc8c420c6532d00d841d
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-08-26 07:46:23 +0100

    Docstrings

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 def makeChange(coins, total):
+    '''
+    Computing number of coins to sum the total
+    
+    Args:
+        coins(int): the array of coins
+        total(int): the total number
+        
+    Returns:
+
+        Number of coins(int): Number of coins needed
+    '''
     if total <= 0:
         return 0
     if len(coins) == 0:
